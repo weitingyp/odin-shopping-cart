ec0aab77a6db5361b6fe606f582fe58a91825443
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2026-03-03 18:44:37 +0800

    feat: replace nav links with NavLink & active styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,21 +1,43 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../data";
+import Home from "./home";
 
 export default function Root() {
 	const cart = useContext(CartContext);
 	return (
 		<div id="container">
 			<nav className="width-full bg-teal-600 flex justify-center gap-5 py-5 text-white font-bold">
-				<a href="" className="hover:cursor-pointer">
+				<NavLink
+					to="/"
+					className={({ isActive }) =>
+						isActive
+							? "text-slate-100 bg-teal-800 rounded-md px-3 py-1"
+							: "px-3 py-1"
+					}
+				>
 					Home
-				</a>
-				<a href="" className="hover:cursor-pointer">
+				</NavLink>
+				<NavLink
+					to="/shop"
+					className={({ isActive }) =>
+						isActive
+							? "text-slate-100 bg-teal-800 rounded-md px-3 py-1"
+							: "px-3 py-1"
+					}
+				>
 					Shop
-				</a>
-				<a href="" className="hover:cursor-pointer">
+				</NavLink>
+				<NavLink
+					to="/cart"
+					className={({ isActive }) =>
+						isActive
+							? "text-slate-100 bg-teal-800 rounded-md px-3 py-1"
+							: "px-3 py-1"
+					}
+				>
 					Cart
-				</a>
+				</NavLink>
 			</nav>
 			<div id="page" className="px-10">
 				<CartContext value={cart}>
