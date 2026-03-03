a542836db8db14e65ec9291da30f2bdc82923a23
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2026-03-03 14:41:04 +0800

    feat: create index router which shows Root element

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import Root from "./routes/root.jsx";
 import "./index.css";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+	},
+]);
+
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Root />
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
