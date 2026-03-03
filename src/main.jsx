9c28856e4d905d22f787f720c48282dc7f93f701
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2026-03-03 14:38:34 +0800

    feat: create Nav bar in Root component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Root from "./routes/root.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <h1 class="m-5 text-green-700">Hello world!</h1>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Root />
+	</StrictMode>,
+);
---
+++ src/routes/root.jsx
@@ -0,0 +1,15 @@
+export default function Root() {
+	return (
+		<nav className="width-full bg-teal-600 flex justify-center gap-5 py-5 text-white font-bold">
+			<a href="" className="hover:cursor-pointer">
+				Home
+			</a>
+			<a href="" className="hover:cursor-pointer">
+				Shop
+			</a>
+			<a href="" className="hover:cursor-pointer">
+				Cart
+			</a>
+		</nav>
+	);
+}
